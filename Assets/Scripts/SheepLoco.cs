8efd0d34269e02f1c49ed726cd0816e07a452ed3
Sanjana Gupta <sanjanagupta@Sanjanas-MacBook-Pro.local>
2017-10-27 20:42:06 -0400

    New Farm Animal Pack added. Sheep model configured. Cat model configured. Sheep animations configured. Sheep move test script added.

---
+++ Assets/Scripts/SheepLoco.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepLoco : MonoBehaviour 
+{
+	public float walkSpeed = 0.01f;
+	public float turnSpeed = 0.01f;
+	Animator anim;
+
+	Rigidbody rb;
+
+	
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	void FixedUpdate () 
+	{
+		float h = Input.GetAxis ("Horizontal1");
+		float v = Input.GetAxis ("Vertical1");
+		anim.SetFloat ("Speed", h);
+		anim.SetFloat ("Direction", v);
+		if (v > 0)
+		Debug.Log ("V: " + v);
+		transform.Translate (Vector3.forward * h*walkSpeed * Time.deltaTime);
+		transform.Rotate (Vector3.up, v *Time.deltaTime);
+	}
+}
